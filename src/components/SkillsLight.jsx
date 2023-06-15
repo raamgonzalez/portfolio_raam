100dd5bc0880cc4c49f54da9f6d4838fb26d79ab
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-06-15 02:17:47 -0300

    Fix: deploy vercel

---
+++ src/components/SkillsLight.jsx
@@ -0,0 +1,14 @@
+import Titles from './Titles'
+
+export default function SkillsLight () {
+  return (
+    <>
+      <Titles title='*SOFTSKILLS' subtitle={null} />
+      <section className='Light'>
+        <p className='Light-01'>Comunicación efectiva, colaboré en coordinación de proyectos</p>
+        <p className='Light-02'>Trabajo en equipo, trabaje en equipos de diseño y proyecto unificados</p>
+        <p className='Light-03'>Resolución de problemas, participe en mesas participativas en los barrios</p>
+      </section>
+    </>
+  )
+}
