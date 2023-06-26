a6a7439633a80bd7368ca77d1ea9145887620d30
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-06-26 14:10:04 -0300

    Fix: arrowButton

--- src/components/ui/ButtonLenguaje.jsx
+++ src/components/ui/ButtonLenguaje.jsx
@@ -12,9 +12,9 @@ export default function ButtonLenguaje () {
 
   return (
     <div className='Button_Language'>
-      <button onClick={(event) => { i18n.changeLanguage('es'); handleChangeActiveLink(event) }} className={activeLink === 'ES' ? 'Button_Language-button active' : 'Button_Language-button blur'}>ES</button>
+      <button onClick={(event) => { i18n.changeLanguage('es'); handleChangeActiveLink(event) }} className={activeLink === 'ES' ? 'Button_Language-button active' : 'Button_Language-button blur-[1px]'}>ES</button>
       <span> / </span>
-      <button onClick={(event) => { i18n.changeLanguage('en'); handleChangeActiveLink(event) }} className={activeLink === 'EN' ? 'Button_Language-button active' : 'Button_Language-button blur'}>EN</button>
+      <button onClick={(event) => { i18n.changeLanguage('en'); handleChangeActiveLink(event) }} className={activeLink === 'EN' ? 'Button_Language-button active' : 'Button_Language-button blur-[1px]'}>EN</button>
     </div>
   )
 }
