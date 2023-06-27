c5c55a45a45346291e296f609a3977e3821b6ab4
Ramsés Gonzalez <gz.ramses.oc@gmail.com>
2023-06-27 15:47:21 -0300

    Fix: buttons in contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,16 @@
 import React, { useContext } from 'react'
 
 import { GlobalContext } from '../context/GlobalContext'
+import ArrowButton from './ui/ArrowButton'
 
 export default function Contact () {
   const { rrss } = useContext(GlobalContext)
   return (
     <div className='Contact'>
-      {/* <p className='Contact-p'>CONTACT ME</p>
-      <StyledGridLine /> */}
+      <ArrowButton />
       <div className='Contact-links'>
-        <a className='Contact-a' href='https://www.linkedin.com/in/ramsesoctaviogonzalez/' target='_blank' rel='noreferrer'>LINKEDIN</a>
-        <a className='Contact-a' href='mailto:gz.ramses.oc@gmail.com' target='_blank' rel='noreferrer'>EMAIL</a>
+        <a className='Contact-a mt-3' href='https://www.linkedin.com/in/ramsesoctaviogonzalez/' target='_blank' rel='noreferrer'>LINKEDIN</a>
+        <a className='Contact-a mt-3' href='mailto:gz.ramses.oc@gmail.com' target='_blank' rel='noreferrer'>EMAIL</a>
       </div>
     </div>
   )
